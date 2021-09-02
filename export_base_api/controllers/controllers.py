fdd45c6d8cf0a13030d4c8c8ba69af5181004f71
andres gonzalez <andresgonzalez@bpsla.com>
2021-09-02 20:51:28 +0000

    api 3

--- export_base_api/controllers/controllers.py
+++ export_base_api/controllers/controllers.py
@@ -5,12 +5,23 @@ import json
 
 class ExportBaseController(http.Controller):
     
+    @http.route('/api/DT', auth='public', method=['GET'], csrf=False)
+    def get_DT(self, **kw):
+        try:
+            DTS = http.request.env['x_detalle_de_trabajo'].sudo().search_read([],['id','name','x_studio_many2one_field_eMpTJ','x_studio_tipo','x_studio_fecha_de_envi','x_studio_notas'])
+            
+            res = json.dumps(DTS, ensure_ascii=False).encode('utf-8')
+            return Response(res, content_type='application/json;charset=utf-8', status=200)
+        except Exception as e:
+            return Response(json.dumps({'error':str(e)}), content_type='application/json;charset=utf-8', status=505)
+    
     @http.route('/api/bases', auth='public', method=['GET'], csrf=False)
     def get_bases(self, **kw):
         try:
-           # bases = http.request.env['helpdesk.ticket'].sudo().search_read([('x_studio_fecha_del_caso',"=",'2021-08-31')],['id','name','x_studio_it_atix','x_studio_dt'])
-            bases = http.request.env['helpdesk.ticket'].sudo().fields_get([('x_studio_fecha_del_caso',"=",'2021-08-31')],['id','name','x_studio_it_atix','x_studio_dt'])
+            bases = http.request.env['helpdesk.ticket'].sudo().search_read([('x_studio_fecha_del_caso',"=",'2021-08-31')],['id','name','x_studio_it_atix','x_studio_dt'])
+            
             res = json.dumps(bases, ensure_ascii=False).encode('utf-8')
             return Response(res, content_type='application/json;charset=utf-8', status=200)
         except Exception as e:
             return Response(json.dumps({'error':str(e)}), content_type='application/json;charset=utf-8', status=505)
+        
