96d6ea00d285084220760300ef71314c4da3988b
andres gonzalez <andresgonzalez@bpsla.com>
2021-09-02 19:15:30 +0000

    api 3

--- export_base_api/controllers/controllers.py
+++ export_base_api/controllers/controllers.py
@@ -8,7 +8,7 @@ class ExportBaseController(http.Controller):
     @http.route('/api/bases', auth='public', method=['GET'], csrf=False)
     def get_bases(self, **kw):
         try:
-            bases = http.request.env['helpdesk.ticket'].sudo().search_read([],['id','name'])
+            bases = http.request.env['helpdesk.ticket'].sudo().search_read([],['id','name','x_studio_it_atix'])
             res = json.dumps(bases, ensure_ascii=False).encode('utf-8')
             return Response(res, content_type='application/json;charset=utf-8', status=200)
         except Exception as e:
