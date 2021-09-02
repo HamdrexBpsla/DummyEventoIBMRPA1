8adaff6855938591aa596d0b0a90d869f77da2bf
andres gonzalez <andresgonzalez@bpsla.com>
2021-09-02 19:54:06 +0000

    api 3

--- export_base_api/controllers/.ipynb_checkpoints/controllers-checkpoint.py
+++ export_base_api/controllers/.ipynb_checkpoints/controllers-checkpoint.py
@@ -8,7 +8,7 @@ class ExportBaseController(http.Controller):
     @http.route('/api/bases', auth='public', method=['GET'], csrf=False)
     def get_bases(self, **kw):
         try:
-            bases = http.request.env['helpdesk.ticket'].sudo().search_read([('x_studio_fecha_del_caso',"=",'2021-08-31')],['id','name','x_studio_it_atix','x_studio_dt.x_studio_tipo'])
+            bases = http.request.env['helpdesk.ticket'].sudo().search_read([('x_studio_fecha_del_caso',"=",'2021-08-31')],['id','name','x_studio_it_atix',related='x_studio_dt.x_studio_tipo'])
             res = json.dumps(bases, ensure_ascii=False).encode('utf-8')
             return Response(res, content_type='application/json;charset=utf-8', status=200)
         except Exception as e:
